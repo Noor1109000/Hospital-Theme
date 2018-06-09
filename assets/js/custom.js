7e6ee4bd78c1aa427a568f8ced157dff99e3c63e
Noor1109000 <pulok.noor@gmail.com>
2018-06-10 02:15:43 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,48 @@
+
+
+
+/* -------screenshot-images--------------------- */
+
+$(document).ready(function(){
+	
+$("#owl-example").owlCarousel({
+	
+	//autoPlay: 3000, //set autoPlay to 3 seconds
+	navigation : true,
+	navigationText: ["<span class='fa fa-angle-left'></span>","<span class=' fa fa-angle-right'></span>"],
+	items : 3,
+	itemsDesktop: [1199,3],
+	itemDesktopSmall : [979,3],
+	  //Pagination
+    pagination : false,
+    paginationNumbers: false,
+    scrollPerPage : true,
+	  //Transitions
+    transitionStyle : true
+ 
+ 
+	
+	
+	
+	
+	});
+
+
+
+
+  
+  
+  
+  
+});
+
+
+
+
+
+
+
+
+
+
+
